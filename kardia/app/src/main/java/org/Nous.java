c2d909e3968dd7e928bad210d6f39b1d7cfdd4c7
gbtsui <gabriel.tsui@protonmail.com>
2025-10-14 17:38:50 -0400

    init
    i have no idea what i am doing lowkey

---
+++ kardia/app/src/main/java/org/Nous.java
@@ -0,0 +1,7 @@
+package org;
+
+public class Nous {
+    public static void main(String[] args) {
+        System.out.println("st gregory palamas please come down and grant this man the nous");
+    }
+}
