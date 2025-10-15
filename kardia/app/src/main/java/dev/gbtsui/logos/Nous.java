62ed223a39206882b8cf8f2aaf2142f3f09db00a
gbtsui <gabriel.tsui@protonmail.com>
2025-10-15 16:08:19 -0400

    feat: figured out how to initialize spring boot

--- kardia/app/src/main/java/dev/gbtsui/logos/Nous.java
+++ kardia/app/src/main/java/dev/gbtsui/logos/Nous.java
@@ -1,7 +1,33 @@
 package dev.gbtsui.logos;
 
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@Configuration @EnableAutoConfiguration @Componentscan
 public class Nous {
     public static void main(String[] args) {
         System.out.println("st gregory palamas please come down and grant this man the nous");
+        SpringApplication.run(Nous.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("beans provided by springboot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 }
